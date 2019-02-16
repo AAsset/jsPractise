90bcb5e0c36340d82da6df15fb32bb693c4beb59
asset <asset.aktay@gmail.com>
2019-02-16 19:41:26 +0600

    load tasks on project and complete thirteenth task

---
+++ project/js/script.js
@@ -0,0 +1,194 @@
+window.addEventListener('DOMContentLoaded', function() {
+    'use strict';
+    let tab = document.querySelectorAll('.info-header-tab'),
+        info = document.querySelector('.info-header'),
+        tabContent = document.querySelectorAll('.info-tabcontent');
+
+    function hideTabContent(a) {
+        for(let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+    }
+    hideTabContent(1);
+
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    }
+
+    info.addEventListener('click', function(event) {
+        let target = event.target;
+        if (target && target.classList.contains('info-header-tab')) {
+            for(let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+    });
+
+    // Timer
+
+    Date.prototype.addDays = function(days) {
+        var today = new Date(this.valueOf());
+        today.setDate(today.getDate() + days);
+        return today;
+    }
+
+    let deadline = new Date().addDays(5);
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor((t/(1000*60*60)));
+
+        return {
+            'total': t,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        }
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timerInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+                hours.textContent = t.hours > 10 ? t.hours : '0' + t.hours;
+                minutes.textContent = t.minutes > 10 ? t.minutes : '0' + t.minutes;
+                seconds.textContent = t.seconds > 10 ? t.seconds : '0' + t.seconds;
+
+            if (t.total <= 0) {
+                clearInterval(timerInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+        }
+    }
+    setClock('timer', deadline);
+
+    // Modal
+
+    let more = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close');
+
+    function showModal() {
+        overlay.style.display = 'block';
+        more.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+    }
+    more.addEventListener('click', function() {
+        showModal();
+    });
+
+    function hideModal() {
+        overlay.style.display = 'none';
+        more.classList.remove('more-splash');
+        document.body.style.overflow = '';
+    }
+    close.addEventListener('click', function() {
+        hideModal();
+    });
+
+    // onclik description-btn show modal
+
+    document.body.addEventListener('click', function(event) {
+        let target = event.target;
+        if (target && target.classList.contains('description-btn')) {
+            showModal();
+        }
+    });
+
+    // Второе задание
+    
+    // let age = document.getElementById('age'),
+    //     userInfo = showUser.bind(age);
+    // userInfo('Иванов', 'Иван');
+	// function showUser(surname, name) {
+	// 	alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+    // }
+
+    // ES6 classes
+
+    class Options {
+        constructor(height, width, bg, fontSize, textAlign) {
+            this.height = height;
+            this.width = width;
+            this.bg = bg;
+            this.fontSize = fontSize;
+            this.textAlign = textAlign;
+        }
+        createDOMElement() {
+            const element = document.createElement('div');
+            const timer = document.querySelector('.timer');
+            timer.appendChild(element);
+            let param = `height:${this.height}px; width:${this.width}px; background-color:${this.bg}; font-size:${this.fontSize}px; text-align:${this.textAlign}`;
+            element.style.cssText = param;
+        };
+    }
+
+    const objOptions = new Options(30, 100, 'red', 18, 'center');
+    objOptions.createDOMElement();
+
+    // Form
+
+    let message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы с вами свяжемся!',
+        failure: 'Что-то пошло не так...'
+    };
+
+    let form = document.querySelector('.main-form'),
+        input = form.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+
+        statusMessage.classList.add('status');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        form.appendChild(statusMessage);
+
+        let request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+        request.setRequestHeader('Conten-Type', 'application/x-www-form-urlencoded');
+
+        let formData = new FormData(form);
+
+        let obj = {};
+        formData.forEach(function(value, key) {
+            obj[key] = value;
+        });
+        let json = JSON.stringify(obj);
+
+        request.send(json);
+
+        request.addEventListener('readystatechange', function() {
+            if (request.readyState < 4) {
+                statusMessage.innerHTML = message.loading;
+            } else if (request.readyState === 4 && request.status === 200) {
+                statusMessage.innerHTML = message.success;    
+            } else {
+                statusMessage.innerHTML = message.failure;
+            }
+        });
+
+        for (let i = 0; i < input.length; i++) {
+            input[i].value = '';
+        }
+    });
+
+});
